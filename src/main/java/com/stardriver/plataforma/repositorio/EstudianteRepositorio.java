c41ad2a06af19e052b6232b3762418ca6e11a036
Harley Roman <166958266+hroman2004@users.noreply.github.com>
2025-07-10 16:02:01 -0500

    Finalizar modelos
    Signed-off-by: hroman2004 <harleyroman2004@gmail.com>

--- src/main/java/com/stardriver/plataforma/repositorio/EstudianteRepositorio.java
+++ src/main/java/com/stardriver/plataforma/repositorio/EstudianteRepositorio.java
@@ -2,6 +2,8 @@ package com.stardriver.plataforma.repositorio;
 
 import com.stardriver.plataforma.entidad.Estudiante;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EstudianteRepositorio extends CrudRepository<Estudiante, Integer> {
 }
--- src/main/java/com/stardriver/plataforma/repositorio/SoporteRepositorio.java
+++ src/main/java/com/stardriver/plataforma/repositorio/SoporteRepositorio.java
@@ -2,6 +2,8 @@ package com.stardriver.plataforma.repositorio;
 
 import com.stardriver.plataforma.entidad.Soporte;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SoporteRepositorio extends CrudRepository<Soporte, Integer> {
 }
